fd8880713ab4c49863d70c2609f434c9ed57c8fa
Salha Siddig <129996035+ssiddig@users.noreply.github.com>
2023-04-16 18:46:47 -0400

    Mortgage Calculator

---
+++ src/main/java/com/MortageCalculator/FutureValue.java
@@ -0,0 +1,29 @@
+package com.MortageCalculator;
+
+import java.util.Scanner;
+public class FutureValue
+      {
+          public static void main(String[] args) {
+
+              Scanner myScanner = new Scanner(System.in);
+
+              System.out.println("Total amount of deposits: ");
+              int deposits = myScanner.nextInt();
+
+
+              System.out.println(" Current interest rate ");
+              double interestRes = myScanner.nextDouble(); // Interest rate answer
+              double InterestRate = interestRes;
+
+
+              System.out.println("Please input the desired length, in years ; ");
+              double numberOfYears = myScanner.nextDouble();
+
+              A = P * (1 + r/n)^(n*t)
+
+              double total = deposits * Math.pow(1 +interestRes / deposits) /
+                      Math.pow(numberOfYears * interestRes);
+
+          }
+
+}
